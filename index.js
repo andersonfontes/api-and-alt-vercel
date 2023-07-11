2809db8e291bc891270df0239b62c2dcf267b844
and <and@and>
2023-07-10 21:08:16 -0300

    index.js arrumado

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// Add Express
-const express = require("express");
-// Initialize Express
-const app = express();
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-// Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
 
 const express = require("express");
 const app = express();
